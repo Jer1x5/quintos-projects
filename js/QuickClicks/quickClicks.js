a11f148dfafc477e66bff020a0354e9e6006d9ea
Jer1x5 <mikhail.tsvetkov07@gmail.com>
2023-05-27 19:37:59 +0300

    1.1.14

--- js/QuickClicks/quickClicks.js
+++ js/QuickClicks/quickClicks.js
@@ -8,25 +8,33 @@ const target = `
 
 const imposter = `
  xx    xx
-  xx  xx
-	  xx
-		xx
-	xx  xx
+  xx  xx 
+	  xx   
+		xx   
+	xx  xx 
  xx    xx
-		`;
+		     `;
+
+async function gameOver() {
+	await alert('You clicked an imposter target, you lost. Do you want to restart?', 12);
+	makeTargets();
+}
 
 // TODO: find the range of row and column values the target can be placed at
 function makeTargets() {
+	erase();
 	let row1 = round(random(1, 23));
 	let col1 = round(random(1, 71));
-	button(target, row1, col1);
+	button(target, row1, col1, makeTargets);
+
 	for (i = 0; i <= 4; i++) {
 		let row2 = round(random(1, 23));
 		let col2 = round(random(1, 71));
-		button(imposter, row2, col2);
+		button(imposter, row2, col2, gameOver);
 	}
 }
 //23 71
-function start() {
+async function start() {
+	await alert('click the target button, if you click an imposter target - you lose', 12);
 	makeTargets();
 }
